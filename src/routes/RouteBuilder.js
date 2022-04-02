6804bed76a9686f17bbd05f10b0e3d9f9a945a30
Jeet-Makadiya-Simform <jeet.m@simformsolutions.com>
2022-04-03 05:00:00 +0530

    fixed: dynamic side navbar

--- src/routes/RouteBuilder.js
+++ src/routes/RouteBuilder.js
@@ -2,18 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Admin from "../pages/admin/Admin";
-import Manager from '../pages/manager/Manager'
+import Manager from "../pages/manager/Manager";
 import Organizer from "../pages/organizer/Organizer";
-import Manager from '../pages/manager/Manager'
 
 const RouteBuilder = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/admin/*" element={<Admin />}></Route> 
-      <Route path="/manager/*" element={<Manager />}></Route> 
+      <Route path="/admin/*" element={<Admin />}></Route>
+      <Route path="/manager/*" element={<Manager />}></Route>
       <Route path="/organizer/*" element={<Organizer />}></Route>
-      <Route path="/manager/*" element={<Manager />}></Route> 
+      <Route path="/manager/*" element={<Manager />}></Route>
     </Routes>
   );
 };
